633ba476e479b7f472e6bacd17b87ee24efcd1bb
Pratik <Pratik@Lenovo-PC>
2020-02-22 13:01:25 +0530

    TCK COLUMN NAme UPDATED

--- MyProj/src/main/java/in/nit/model/Student.java
+++ MyProj/src/main/java/in/nit/model/Student.java
@@ -1,11 +1,20 @@
 package in.nit.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class Student {
+	@Id
+	@Column(name="sid")
 	private int sid;
+	@Column(name="sname")
 	private String sname;
+	@Column(name="sfee")
 	private String sfee;
 	
 	
